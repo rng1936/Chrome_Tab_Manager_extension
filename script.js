9a0d8b19a54f5d42ead2f901ba77d605b6a02c13
Ronald Ng <ronaldn1936@gmail.com>
2023-01-24 18:52:40 -0500

    changes

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ let saveButton = document.getElementById("saveButton");
 let saveAllButton = document.getElementById("saveAll");
 let loadTabButton = document.getElementById("loadTabs")
 let clearTabButton = document.getElementById("clearTabs");
+let editFolder = document.getElementById("editFolders");
 let tabList = document.getElementById("contents");
+
+let editSaveFolds = false;
+let fold1 = document.getElementById("f1");
+let fold2 = document.getElementById("f2");
+let fold3 = document.getElementById("f3");
+let fold4 = document.getElementById("f4");
+let fold5 = document.getElementById("f5");
+let fold6 = document.getElementById("f6");
 
 saveButton.onclick = () => {
     chrome.runtime.sendMessage({message: "Save"});
@@ -71,3 +80,27 @@ chrome.runtime.onMessage.addListener(
             tabList.appendChild(anchor);
         }
 })
+
+editFolder.onmouseover = () => {
+    options.innerHTML = "Edit/Save Folder Names";
+}
+
+editFolder.onmouseleave = () => {
+    options.innerHTML = "Tab Manager";
+}
+
+editFolder.onclick = () => {
+    editSaveFolds = !editSaveFolds;
+    fold1.disabled = !fold1.disabled;
+    fold2.disabled = !fold2.disabled;
+    fold3.disabled = !fold3.disabled;
+    fold4.disabled = !fold4.disabled;
+    fold5.disabled = !fold5.disabled;
+    fold6.disabled = !fold6.disabled;
+}
+
+fold1.onclick = () => {
+    if (editSaveFolds) {
+        
+    }
+}
